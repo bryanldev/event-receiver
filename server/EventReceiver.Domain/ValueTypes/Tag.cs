811f96c51e86839b624980dc1d5087bd0d9f588d
bryanlds <bryanlds5@gmail.com>
2020-09-01 08:48:59 -0300

    refactoring

--- server/EventReceiver.Domain/ValueTypes/Tag.cs
+++ server/EventReceiver.Domain/ValueTypes/Tag.cs
@@ -1,4 +1,5 @@
 ﻿using Flunt.Notifications;
+using Flunt.Validations;
 using System.Text.RegularExpressions;
 
 namespace EventReceiver.Domain.ValueTypes
@@ -7,34 +8,56 @@ namespace EventReceiver.Domain.ValueTypes
     {
         public string Country { get; private set; }
         public string Region { get; private set; }
-        public string Name { get; private set; }
+        public string SensorName { get; private set; }
 
 
         public Tag(string country, string region, string name)
         {
             Country = country;
             Region = region;
-            Name = name;
+            SensorName = name;
+            Validate();
         }
 
         public Tag(string tag)
         {
+            if(string.IsNullOrWhiteSpace(tag))
+            {
+                AddNotification(nameof(Tag), "Tag should not be empty.");
+                return;
+            }
+
             string[] splitTag;
             splitTag = Regex.Split(tag, "\\.");
-            if (splitTag.Length == 3)
+
+            if (splitTag.Length != 3)
+            {
+                AddNotification(nameof(Tag), "Unexpected tag format.");
+                return;
+            }
+
+            if (Valid)
             {
                 Country = splitTag[0];
                 Region = splitTag[1];
-                Name = splitTag[2];
+                SensorName = splitTag[2];
+                Validate();
             }
-            else
-                AddNotification("Tag", "Unexpected tag format.");
+        }
+
+        public void Validate()
+        {
+            AddNotifications(new Contract()
+                .IsNotNull(Country, "Tag.Country", "Country should not be null")
+                .IsNotNull(Region, "Tag.Region", "Region should not be null")
+                .IsNotNull(SensorName, "Tag.SensorName", "SensorName should not be null")
+                );
         }
 
         public override string ToString()
         {
 
-            return $"{Country}.{Region}.{Name}";
+            return $"{Country}.{Region}.{SensorName}";
 
         }
     }
--- server/EventReceiver.Test/ValueTypes/TagTest.cs
+++ server/EventReceiver.Test/ValueTypes/TagTest.cs
@@ -3,6 +3,7 @@ using EventReceiver.Domain.ValueTypes;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace EventReceiver.Domain.ValueTypes.Tests
 {
@@ -16,49 +17,76 @@ namespace EventReceiver.Domain.ValueTypes.Tests
         [Test()]
         public void TagIsValid()
         {
+            // Arrange
             var tag = new Tag("Brasil.Sudeste.Sensor01");
-            Assert.AreEqual(true, tag.Valid);
+
+            // Assert
+            Assert.True(tag.Valid);
         }
 
         [Test()]
         public void TagIsInvalid1()
         {
+            // Arrange
             var tag = new Tag("Brasil.Sudeste.Sensor01.Radix");
-            Assert.AreEqual(true, tag.Invalid);
+
+            // Assert
+            Assert.True(tag.Invalid);
+            Assert.AreEqual("Unexpected tag format.", tag.Notifications.Single().Message);
         }
 
 
         [Test()]
         public void TagIsInvalid2()
         {
+            // Arrange
             var tag = new Tag("Brasil.Sudeste");
-            Assert.AreEqual(true, tag.Invalid);
+
+            // Assert
+            Assert.True(tag.Invalid);
+            Assert.AreEqual("Unexpected tag format.", tag.Notifications.Single().Message);
         }
 
         [Test()]
         public void TagIsInvalid3()
         {
+            // Arrange
             var tag = new Tag("Brasil");
-            Assert.AreEqual(true, tag.Invalid);
+
+            // Assert
+            Assert.True(tag.Invalid);
+            Assert.AreEqual("Unexpected tag format.", tag.Notifications.Single().Message);
         }
 
         [Test()]
         public void EmptyArgumentTagTest()
         {
+            // Arrange
             var tag = new Tag("");
-            Assert.AreEqual(true, tag.Invalid);
+
+            // Assert
+            Assert.True(tag.Invalid);
+            Assert.AreEqual("Tag should not be empty.", tag.Notifications.Single().Message);
         }
 
         [Test()]
         public void NullArgumentTagTest()
         {
-            Assert.That(()=> new Tag(null), Throws.ArgumentNullException);
+            // Arrange
+            var tag = new Tag(null);
+
+            // Assert
+            Assert.True(tag.Invalid);
+            Assert.AreEqual("Tag should not be empty.", tag.Notifications.Single().Message);
         }
 
         [Test()]
         public void ToStringTest()
         {
+            // Arrange
             var tag = new Tag("Brasil.Norte.Sensor02");
+
+            // Assert
             Assert.That(tag.ToString, Is.EqualTo("Brasil.Norte.Sensor02"));
         }
     }
