c9de9403fe7d5bb925f6811ae3b14ef63b2605f5
bryanlds <bryanlds5@gmail.com>
2021-02-28 23:53:50 -0300

    Delete old project

--- server/EventReceiver.Domain/Models/SensorEventModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EventReceiver.Domain.Models
-{
-    public class SensorEventModel
-    {
-        public string Timestamp { get; set; }
-        public string Tag { get; set; }
-        public string Valor { get; set; }
-    }
-}
